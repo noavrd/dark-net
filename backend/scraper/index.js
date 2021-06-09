59a4c7375b679db59ac9f65141c90470a0afc2da
noavrd <noavrd@gmail.com>
2021-06-09 18:00:40 +0300

    take all the information from the website

--- backend/scraper/index.js
+++ backend/scraper/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 let browser;
 const defaultProxyUrl = '9050';
 const runWithProxy = [
@@ -24,11 +25,39 @@ async function createScraper() {
   let page = await browser.newPage();
   console.log(`Navigating to ${url}...`);
   try {
-    console.log(1);
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
-    console.log(2);
-    const response = await page.$$eval('div.col-sm-12', (data) => data);
-    console.log(response);
+    // const response = await page.$$eval('div.pre-header', (data) => data);
+    const content = await page.content();
+    const $ = cheerio.load(content);
+    const titles = [];
+    const contents = [];
+    const authors = [];
+    const dates = [];
+
+    $('.col-sm-5').each((idx, elem) => {
+      const title = $(elem).text();
+      titles.push(title);
+    });
+    $('.text').each((idx, elem) => {
+      const eachContent = $(elem).text();
+      contents.push(eachContent);
+    });
+    let count = 0;
+    $('.col-sm-6').each((idx, elem) => {
+      if (count % 2 === 0) {
+        let authorAndDate = $(elem).text();
+        let arr = [];
+        arr = authorAndDate.split(' ');
+        authors.push(arr[2]);
+        dates.push({
+          day: arr[4],
+          month: arr[5],
+          year: arr[6],
+          time: arr[7],
+        });
+      }
+      count++;
+    });
   } catch (err) {
     console.log(err);
   }
