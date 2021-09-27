30d75ead11af84796d55e3cdd654faa089dfecd5
noavrd <noavrd@gmail.com>
2021-09-28 01:32:29 +0300

    Update Pagination.js

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -9,28 +9,43 @@ export default function Pagination({
   const [pages] = useState(Math.round(allPastes.length / dataLimit));
   const [currentPage, setCurrentPage] = useState(1);
 
+  //Go to the next page
   function goToNextPage() {
     setCurrentPage((page) => page + 1);
   }
 
+  //Go to the previous page
   function goToPreviousPage() {
     setCurrentPage((page) => page - 1);
   }
 
+  //Go to any page
   function changePage(item) {
     const pageNumber = Number(item);
     setCurrentPage(pageNumber);
   }
 
+  //Get the pastes according to the page
   const getPaginatedData = () => {
     const startIndex = currentPage * dataLimit - dataLimit;
     const endIndex = startIndex + dataLimit;
     return allPastes.slice(startIndex, endIndex);
   };
 
+  //Get the number of the pages to show
   const getPaginatedGroup = () => {
     let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
-    return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+    const showPages = new Array(pageLimit)
+      .fill()
+      .map((_, idx) => start + idx + 1);
+
+    const found = showPages.find((elem) => elem > pages);
+
+    if (!found) {
+      return showPages;
+    }
+    const spliceIndex = showPages.findIndex((elem) => elem > pages);
+    return showPages.splice(0, spliceIndex);
   };
 
   return (
